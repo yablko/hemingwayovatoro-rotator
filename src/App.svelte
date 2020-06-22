7fbbef4fbde1ec1cb42bf1111251e465c7c75c50
yablko <yablko@brm.sk>
2020-06-22 12:42:18 +0200

    heck yea, part 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 <script>
-let bookTitle = "I'm gonna shoot my dick off, with guns"
+// components
+import Wave from './components/Wave.svelte'
+
+let bookTitle = "Itchy Scrotum"
+let waving = false
 </script>
+
+<style>
+	label {
+		position: absolute;
+		top: 1em;
+		left: 1em;
+		font-size: 0.7em;
+	}
+</style>
 
 <header>
 	<h1>HEMINGWAYOVÁTORO-ROTÁTOR™</h1>
+
+	<label for="waving">
+		<input type="checkbox" id="waving" bind:checked={waving}>
+		waving
+	</label>
 </header>
 
 <main>
@@ -15,5 +33,8 @@ let bookTitle = "I'm gonna shoot my dick off, with guns"
 </main>
 
 <footer class="water">
-	<!--  -->
+	<Wave where="back" animation="big" {waving} />
+	<Wave where="middle" animation="tiny" {waving} />
+	<img class="hem" src="./assets/hemingway.png" alt="my boy ernest" />
+	<Wave where="front" animation="small" {waving} />
 </footer>
