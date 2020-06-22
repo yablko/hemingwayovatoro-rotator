594d6d18233d0ea14839d55799c3efe66b1b9a98
yablko <yablko@brm.sk>
2020-06-22 12:33:22 +0200

    heck yea

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+let bookTitle = "I'm gonna shoot my dick off, with guns"
+</script>
+
+<header>
+	<h1>HEMINGWAYOVÁTORO-ROTÁTOR™</h1>
+</header>
+
+<main>
+	<h2>{bookTitle}</h2>
+
+	<section class="controls">
+		<!--  -->
+	</section>
+</main>
+
+<footer class="water">
+	<!--  -->
+</footer>
